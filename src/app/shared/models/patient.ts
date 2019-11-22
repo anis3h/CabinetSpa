800869b782350435a8bd2d46cde1d30ada1a130c
Gerry Weber <gerryweber@o2online.de>
2019-11-22 15:34:26 +0100

    added Age to Informations

--- src/app/shared/models/patient.ts
+++ src/app/shared/models/patient.ts
@@ -6,11 +6,5 @@ class Patient {
   tel: string;
   adresse: string;
   dateOfBirth: Date;
-  private _test: string;
-  get test(): Date {
-    return this.dateOfBirth;
-  }
-  // set test(value: string) {
-  //     this._test = value;
-  // }
+  age: Age;
 }
